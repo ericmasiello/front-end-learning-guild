90f49dde9f2dc4e3bda17f3cb4b7c951f5c8e898
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-02 13:20:13 -0500

    Add example 2 for loaders

---
+++ 04_webpack/examples/02_using_loaders/js/index.js
@@ -0,0 +1,15 @@
+import model from './model';
+import buildTodoList from './listView';
+
+const todoList = buildTodoList(model.todos);
+const completedList = buildTodoList(model.completedTodos);
+
+const root = document.getElementById('root');
+
+root.innerHTML = `
+  <h2>Incomplete</h2>
+  ${todoList.outerHTML}
+
+  <h2>Complete</h2>
+  ${completedList.outerHTML}
+`;
---
+++ 04_webpack/examples/02_using_loaders/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      }
+    ],
+  }
+}
