b3c6f83903f10b15ba52038440afe8546c32ba39
Eric Masiello <eric.j.masiello@gmail.com>
2018-02-03 08:55:28 -0500

    Add example 03

---
+++ 04_webpack/examples/03_using_plugins/src/model.js
@@ -0,0 +1,17 @@
+const todos = [
+  'Front-end Tooling',
+  'Webpack',
+  'Babel',
+];
+
+const completedTodos = [
+  'Jest',
+  'this in JS',
+];
+
+const model = {
+  todos,
+  completedTodos,
+};
+
+export default model;
